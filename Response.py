d4c0cc9f772d660a007b867f0d0fa8074608ed6e
Youssef Farag <88057098+JoeFarag-00@users.noreply.github.com>
2023-03-06 01:29:40 +0200

    added "List Members" response

--- Response.py
+++ Response.py
@@ -1,19 +1,21 @@
 import random
 
-def get_response(message: str) -> str:
+def get_response(message: str, attendees) -> str:
     
     if message.lower() == 'hello':
-        return 'hi'
-    
-    elif message.lower() == 'hello':
         return 'Hey there!'
     
-    elif message.lower() == 'Mina' or 'mina':
-        MinaList = ["Wese5", "Eltayeb", "نجسسس", "وصخ", "رخيص"]
-        return MinaList[random.randint(1, 6)]
+    elif message.lower() == 'list members':
+        attend = ', '.join(list(attendees.keys()))
+        return attend
     
-    elif message.lower() == '!help':
-        return '`This is a help message that you can modify.`'
+    elif message.lower() == 'help':
+        return "There are 2 ways to set attendance: \n1. Upload the QR code and the Person's name. \n2. Use the 'AL:' (link here) , 'Person'.\n \n if you need an example TYPE: 'Show'."
+         
+    elif message.lower() == 'mina':
+        MinaList = ["Hala","Wese5", "Eltayeb", "نجسسس", "وصخ", "رخيص", "Btk"]
+        return MinaList[random.randint(1, 5)]
     
-    else:
-        return None
+    elif message.lower() == 'show':
+        return "There are 2 ways to set attendance: \n1. 'upload Pic' 'Mina'. \n2. AL: https://e-learning.msa.edu.eg/mod/attendance/attendance.php?qrpass=b50zfz&sessid=687603 , Mina."
+    
